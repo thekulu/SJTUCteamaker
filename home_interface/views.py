4e2e84a55f49f3116d04b5ca4ed987c1c3d01ee0
W1sh222 <1400151410@sjtu.edu.cn>
2023-10-24 17:07:23 +0800

    实现了调用数据库以及添加讨论的功能

--- home_interface/views.py
+++ home_interface/views.py
@@ -16,6 +16,39 @@ def home(request):
     return render(request, "home.html")
 
 def blog(request):
+    if request.method == 'POST':
+
+        # 获取POST数据
+        name = request.POST.get('name')
+        url = request.POST.get('url')
+        # 获取其他表单字段
+        start_regi = request.POST.get('start_regi')
+        end_regi = request.POST.get('end_regi')
+        start_compt = request.POST.get('start_compt')
+        number = request.POST.get('number')
+        intro = request.POST.get('intro')
+        category = request.POST.get('category')
+
+
+        # 检查 content 是否存在
+        if name:
+            # 创建 new_competition 对象并将其保存到数据库
+            new_competition = Competition(
+                    name=name,
+                    url=url,
+                    start_regi=start_regi,
+                    end_regi=end_regi,
+                    start_compt=start_compt,
+                    number=number,
+                    intro=intro,
+                    category=category
+                )
+            new_competition.save()
+
+            # 返回响应，可以是重定向或其他响应
+            return HttpResponse('讨论已发布')
+
+
     competition = Competition.objects.all()  # 查询数据库中的所有Discussion对象
     return render(request, "blog.html",  {'competition': competition})
 
@@ -47,46 +80,49 @@ def blogs(request, competition_id):
 # def blogs(request, competition_id):
 #     # 创建一个新的 User 实例
 #     new_user = User(
-#         user_name="Bob",
-#         user_id=2,  # 你需要选择一个唯一的学号
+#         user_name="开心超人",
+#         user_id=3,  # 你需要选择一个唯一的学号
 #         user_password="2003",
-#         profile="profile_picture.jpg",  # 上传的头像文件路径
-#         bio="Self-introduction",
-#         major="Computer Science"
+#         profile="images/1成员1.jpeg",  # 上传的头像文件路径
+#         bio="我是开心超人",
+#         major="Attaker Science"
 #     )
 
 #     # 保存该实例到数据库
 #     new_user.save()
 #     # 创建一个新的 Competition 实例
 #     new_competition = Competition(
-#         name="“挑战杯” 全国大学生课外学术科技作品竞赛",
-#         image="img/blog/blog-1.jpg",  # 上传的竞赛图片文件路径
+#         name="全国大学生数学竞赛",
+#         image="images/数学.png",  # 上传的竞赛图片文件路径
 #         start_regi="2023-11-01",
-#         end_regi="2023-11-25",
+#         end_regi="2023-12-25",
 #         start_compt="2023-11-30",
-#         url="https://tiaozhanbei.com",
+#         url="http://www.cmathc.cn/",
 #         number=100,
-#         intro="“挑战杯” 系列竞赛被誉为中国大学生学术科技 “奥林匹克” ，是国内大学生最关注最热门的全国性竞赛，也是全国最具代表性、权威性、示范性、导向性的大学生竞赛。该竞赛每两年举办一次，旨在鼓励大学生勇于创新、迎接挑战的精神，培养跨世纪创新人才。",
+#         intro="全国大学生数学竞赛,非常challenge",
 #         category=1  # 使用合适的类别值
 #     )
 
 #     # 保存该实例到数据库
 #     new_competition.save()
-#     # 创建一个新的 Team 实例
-#     new_team = Team(
-#         name="开心超人队",
-#         competition=new_competition,  # 将团队与特定竞赛相关联
-#         creator=new_user,  # 将团队的创建者设置为特定用户
-#     )
+#     # # 创建一个新的 Team 实例
+#     # new_team = Team(
+#     #     name="超人队",
+#     #     competition=new_competition,  # 将团队与特定竞赛相关联
+#     #     creator=new_user,  # 将团队的创建者设置为特定用户
+#     # )
 
-#     # 保存该实例到数据库
-#     new_team.save()
-#     # 创建一个新的 Discussion 实例
-#     new_discussion = Discussion(
-#         content="good good",
-#         user=new_user,  # 将讨论与特定用户相关联
-#         competition=new_competition,  # 将讨论与特定竞赛相关联
-#     )
-#     # 保存该实例到数据库
-#     new_discussion.save()
-#     return render(request, 'blog-single.html', {'competition': new_competition, 'discussions': new_discussion, 'teams': new_team})
+#     # # 保存该实例到数据库
+#     # new_team.save()
+#     # # 创建一个新的 Discussion 实例
+#     # new_discussion = Discussion(
+#     #     content="good good",
+#     #     user=new_user,  # 将讨论与特定用户相关联
+#     #     competition=new_competition,  # 将讨论与特定竞赛相关联
+#     # )
+#     # # 保存该实例到数据库
+#     # new_discussion.save()
+#     competition = Competition.objects.get(pk=competition_id)
+#     teams = Team.objects.filter(competition=competition)
+#     discussions = Discussion.objects.filter(competition=competition)
+#     return render(request, 'blog-single.html', {'competition': new_competition, 'discussions': discussions, 'teams': teams})
