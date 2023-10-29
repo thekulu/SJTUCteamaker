c30e182dc7211ee0ace73e8e9c8fbe13d7c285ce
W1sh222 <1400151410@sjtu.edu.cn>
2023-10-29 12:03:45 +0800

    111

--- main_interface/views.py
+++ main_interface/views.py
@@ -61,75 +61,46 @@ def custom_login(request):
     return render(request, 'login.html')
 
 def personal_add(request):
+    user = request.user
+    if not user.is_authenticated:
+        return redirect('/login/')
     if request.method == 'POST':
-        user_name = request.POST.get('user_name')
-        user_password = request.POST.get('user_password')
-        user_id = request.POST.get('user_id')
-        user_grade = request.POST.get('user_grade')
-        user_email = request.POST.get('user_email')
-        profile = request.FILES.get('profile')  # 头像文件
+        try:
+            user_name = request.POST.get('user_name')
+            # user_password = request.POST.get('user_password')
+            # user_id = request.POST.get('user_id')
+            user_grade = request.POST.get('grade')
+            user_email = request.POST.get('email')
+            major = request.POST.get('major')
+            bio = request.POST.get('bio')
+            profile = request.FILES.get('profile')  # 头像文件
 
-        if profile:
-            # 保存文件到media目录中
-            file_name = default_storage.save('profile_pictures/' + profile.name, ContentFile(profile.read()))
+            if profile:
+                print("change")
+                # 保存文件到media目录中
+                file_name = default_storage.save('profile_pictures/' + profile.name, ContentFile(profile.read()))
 
-            # 获取文件的URL
-            file_url = 'profile_pictures/' + profile.name
+                # 获取文件的URL
+                file_url = 'profile_pictures/' + profile.name
+            else:
+                file_url = user.profile
 
-        # 创建用户并保存到数据库
-        user = User(
-            user_name=user_name,
-            user_password=user_password,
-            user_id=user_id,
-            user_grade=user_grade,
-            user_email=user_email,
-            profile=file_url  # 上传的头像
-        )
+            # 创建用户并保存到数据库
+        
+            user.user_name=user_name
+            user.user_grade=user_grade
+            user.user_email=user_email
+            user.major=major
+            user.profile=file_url
+            user.bio=bio
+        except:
+            messages.error(request, '请正确输入')
+
         user.save()
 
-        # 设置用户已认证为 True
-        user.is_authenticated = True
-        user.save()
-
-        messages.success(request, '用户添加成功')
+        # print(user.user_grade,user.user_email)
         return redirect('/personal/')  # 重定向到用户个人资料页面或其他页面
-    
-def personal_add(request):
-    # 默认为 POST 表单    
-    name = request.POST.get("name")
-    url = request.POST.get("url")
-    inputfile = request.FILES.get('inputfile')
-    number = request.POST.get("number")
-    
-    # 竞赛类别单选项
-    category = request.POST.get("category")
 
-    start_regi = request.POST.get("start_regi")
-    end_regi = request.POST.get("end_regi")
-    start_compt = request.POST.get("start_compt")
-    intro = request.POST.get("intro")
-
-    # 处理上传的文件
-    if inputfile:
-        # 保存文件到media目录中
-        file_name = default_storage.save('profile_pictures/' + inputfile.name, ContentFile(inputfile.read()))
-
-        # 获取文件的URL
-        file_url = 'profile_pictures/' + inputfile.name
-
-    Competition.objects.create(name = name,
-                                url = url, 
-                                image = file_url,
-                                number = number,
-                                category = category,
-                                start_regi = start_regi,
-                                end_regi = end_regi,
-                                start_compt = start_compt,
-                                intro = intro
-                                )
-    # return HttpResponse("添加成功")
-    messages.error(request, '添加成功')
-    return redirect("/personel/") # urls.py 中的 personel.html 地址
 
 # Create your views here.
 def index(request):
